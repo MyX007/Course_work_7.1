34af33446e7537cce562ce8566ca25801dd4e32f
Vladimir Blyntsev <blyncev@yandex.ru>
2025-05-22 20:31:51 +0700

    1

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.10
+
+WORKDIR /config
+
+COPY requirements.txt ./
+RUN apt-get update \
+    && apt-get install -y gcc libpq-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+ENV CELERY_BROKER_URL='redis://localhost:6379/0'
+ENV CELERY_BACKEND='redis://localhost:6379/0'
+ENV DATABASE_URL=postgres://postgres:112233@host:2255/Docker
+ENV SECRET_KEY=django-insecure-rt4@$e0j=orc&=v=39fu2aw6r6ey7r4=ntx46sev5zts^l=93y
+ENV DEBUG=False
+
+RUN mkdir -p /media
+
+EXPOSE 8000
+
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ habits_tracker/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from config.settings import HABIT_FREQUENCY
+
+from users.models import User
+
+
+class Day(models.Model):
+    day = models.CharField(max_length=100, unique=True, verbose_name="День недели")
+
+
+class Habit(models.Model):
+    """Класс привычки."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='habits',
+                             null=True, blank=True)
+    time = models.DateTimeField(verbose_name='Время', null=True, blank=True)
+    place = models.CharField(max_length=50, verbose_name='Место')
+    action = models.CharField(max_length=50, verbose_name='Действие')
+    pleasent = models.BooleanField(verbose_name='Признак приятной привычки')
+    publicity = models.BooleanField(verbose_name='Признак публичности')
+    execution_time = models.PositiveIntegerField(default=0, verbose_name='Время на выполнение')
+    reward = models.CharField(max_length=50, verbose_name='Награда', blank=True, null=True)
+    related_habits = models.ForeignKey(
+        'self',
+        verbose_name='Связанная привычка',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    frequency = models.CharField(
+        choices=HABIT_FREQUENCY,
+        verbose_name="frequency",
+        default="m h * * *",
+        blank=True,
+        null=True,
+    )
+    days_of_week = models.ManyToManyField(
+        Day,
+        null=True,
+        blank=True,
+        verbose_name="День недели"
+    )
+    end_time = models.DateTimeField(
+        verbose_name="Время последнего выполнения привычки за день",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
